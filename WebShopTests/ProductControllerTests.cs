b1270af2dfe2eb346dcc7888af068b5df41a5f08
Bjornanger <143402834+Bjornanger@users.noreply.github.com>
2024-11-19 12:16:52 +0100

    Test= GetAllProducts

--- WebShopTests/ProductControllerTests.cs
+++ WebShopTests/ProductControllerTests.cs
@@ -4,39 +4,58 @@ using Xunit;
 using WebShop;
 using WebShop.Controllers;
 using WebShopSolution.DataAccess.Entities;
+using WebShopSolution.DataAccess.Repositories;
 using WebShopSolution.Shared.Interfaces;
 
 
 using WebShopSolution.DataAccess.Repositories.Products;
+using WebShopSolution.DataAccess.UnitOfWork;
+using System.Collections.Generic;
 
 public class ProductControllerTests
 {
+    private readonly IUnitOfWork _unitOfWork = A.Fake<IUnitOfWork>();
     private readonly IProductRepository _productRepository = A.Fake<IProductRepository>();
     private readonly ProductController _controller;
 
     public ProductControllerTests()
     {
         _productRepository = A.Fake<IProductRepository>();//Kolla om detta stämmer från övningsuppgifterna
+        _unitOfWork = A.Fake<IUnitOfWork>();
 
-        // Ställ in mock av Products-egenskapen
+        
+
+        _controller = new ProductController(_unitOfWork);
+
     }
 
     [Fact]
-    public void GetProducts_ReturnsOkResult_WithAListOfProducts()
+    public async void GetProducts_ReturnsOkResult_WithAListOfProducts()
     {
         // Arrange
+        var productListOfDummies = A.CollectionOfDummy<Product>(3);
 
-        var product = A.Dummy<Product>();
 
-        _productRepository.Add(product);
+        A.CallTo(() => _unitOfWork.Repository<Product>()).Returns(_productRepository);
 
-        A.CallTo(() => _productRepository.GetAll());
+        A.CallTo(() => _productRepository.GetAllAsync())
+            .Returns(Task.FromResult((IEnumerable<Product>)productListOfDummies));
+        
         // Act
-        var prodList = _productRepository.GetAll();
+        var result = await _controller.GetAllProducts();
+
 
         // Assert
-        Assert.NotNull(prodList);
-        A.CallTo(() => _productRepository.GetAll()).MustHaveHappened();
+        var actionResult = Assert.IsType<ActionResult<IEnumerable<Product>>>(result);
+        var okResult = actionResult.Result as OkObjectResult;
+        Assert.NotNull(okResult);
+        var returnValue = Assert.IsType<List<Product>>(okResult.Value);
+
+        Assert.Equal(productListOfDummies, returnValue);
+        Assert.Equal(3, returnValue.Count);
+
+        A.CallTo(() => _productRepository.GetAllAsync()).MustHaveHappened();
+
     }
 
     [Fact]
@@ -46,8 +65,8 @@ public class ProductControllerTests
         var product = A.Dummy<Product>();
 
         // Act
-        _productRepository.Add(product);
+        _productRepository.AddAsync(product);
         // Assert
-        A.CallTo(() => _productRepository.Add(product)).MustHaveHappened();
+        A.CallTo(() => _productRepository.AddAsync(product)).MustHaveHappened();
     }
 }
