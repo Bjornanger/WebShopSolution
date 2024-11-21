635c6304d96ae007ab4895aa0d571748104d0cd1
Bjornanger <143402834+Bjornanger@users.noreply.github.com>
2024-11-21 12:38:03 +0100

    Added Migration - Customer new Props

--- WebShopSolution.DataAccess/Entities/Customer.cs
+++ WebShopSolution.DataAccess/Entities/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 using WebShopSolution.Shared.Interfaces;
 
@@ -8,10 +9,15 @@ public class Customer: IEntity
 {
     [Key]
     public int  Id { get; set; }
+    [Required]
     public string FirstName { get; set; }
-
+    [Required]
     public string LastName { get; set; }
-    
+    [EmailAddress]
+    public string Email { get; set; }
+   
+    public string Password { get; set; }
+
     [JsonIgnore]
     public ICollection<Order> Orders { get; set; } = new List<Order>();
 
